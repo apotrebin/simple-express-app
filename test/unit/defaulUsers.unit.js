cb512488aec0041b320cd55c1742b88d7aa9fa59
Akram Potrebin <imtogo@mail.ru>
2019-04-01 03:04:00 +0600

    backup (#7)
    * tests
    
    * test
    
    * test
    
    * readme (#6)
    
    * readme

--- test/unit/defaulUsers.unit.js
+++ test/unit/defaulUsers.unit.js
@@ -1,4 +1,5 @@
 const usersObj = require('../../store/store');
+const itParam = require('mocha-param');
 const assert = require('chai').assert;
 
 
@@ -6,11 +7,23 @@ describe('Default users', function(){
 
     it('assert usersObj.list is an array', function(){
      assert.typeOf(usersObj.list, 'array');
+    });
+
+    it('assert usersObj.list length is 3', function(){
      assert.equal(usersObj.list.length, 3, 'There are not 3 default user');
     });
 
-    it('assert usersObj.list length is 3  ', function(){
-     assert.equal(usersObj.list.length, 3, 'There are not 3 default user');
+    describe('Assert all default users in storage', function () {
+      const params = [
+                      {"_id":1, "name":"John Doe",        "role":"admin",       "email":"john@app.io"},
+                      {"_id":2, "name":"Jaskaran Nixon",  "role":"user",        "email":"jaskaran@app.io"},
+                      {"_id":3, "name":"Tyler Pitt",      "role":"accountant",  "email":"tyler@app.io"}
+                    ];
+      itParam(`assert current user`, params, async function (param) {
+        const paramIndex = param._id-1;
+        assert.equal(usersObj.list[paramIndex].name, param.name);
+        assert.equal(usersObj.list[paramIndex].role, param.role);
+        assert.equal(usersObj.list[paramIndex].email, param.email);
+      });
     });
-
 });
